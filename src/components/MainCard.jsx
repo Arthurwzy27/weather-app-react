8c00215857e7016bb425f212e15376e570916a2b
Arthur Picard <arthur.picard7@gmail.com>
2022-02-09 18:08:43 +0100

    Update: .env file created

--- src/components/MainCard.jsx
+++ src/components/MainCard.jsx
@@ -5,8 +5,8 @@ const Maincard = () => {
 
   const [lat, setLat] = useState('41.39')
   const [lon, setLon] = useState('2.15')
-  const [cityName, setCityName] = useState('Barcelona')
-  const API_key = '9c7f34b95b5c80f28c782184dd6ca42b'
+  const [cityName, setCityName] = useState('')
+
 
   useEffect(() => {
     fetch(`api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&appid=${API_key}`)
