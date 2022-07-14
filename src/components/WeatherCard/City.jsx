04a766f3f6caef7b720db28f3ab9f7c76974a0f0
Arthur Picard <arthur.picard7@gmail.com>
2022-07-14 14:47:24 +0200

    City component created with props to get general infos

---
+++ src/components/WeatherCard/City.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './City.scss';
+
+const City = ({weatherData}) => {
+  return (
+    <div>
+      <img src="" alt='country-flag' />
+      <h2>{weatherData.city.name} ({weatherData.city.country})</h2>
+      <h3>{weatherData.list[0].main.temp}°</h3>
+      <img src="" alt={weatherData.list[0].weather[0].description} />
+      <p>{weatherData.list[0].weather[0].description}</p>
+      <p>Min: 28°</p>
+      <p>Max: 34°</p>
+      <p>Wind: 8km/h WE</p>
+      {/* <img src="" alt='country-flag' />
+      <h2>Paris</h2>
+      <h3>32°</h3>
+      <img src="" alt="Sunny" />
+      <p>Sunny</p>
+      <p>Min: 28°</p>
+      <p>Max: 34°</p>
+      <p>Wind: 8km/h WE</p> */}
+    </div>
+  )
+}
+
+export default City
